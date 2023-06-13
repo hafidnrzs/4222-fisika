447a7038e7a64f5d8d04a1ad04c82d899ba542f7
hafidnrzs <hafidnurazis@gmail.com>
2023-06-13 23:26:23 +0700

    Gabungkan fungsi ke pustaka
    - Gabungkan fungsi utama dan fungsi konversi dalam satu file pustaka.cpp
    - Ubah semua tipe data menjadi double
    - Update fungsi prototype pada header files

--- src/kecepatan/header.h
+++ src/kecepatan/header.h
@@ -2,6 +2,22 @@
 using namespace std;
 
 // Fungsi prototype
-float konversiSekonKeJam (float);
-float konversiSekonKeMenit (float);
-float cariKecepatan(float, float);
+// Fungsi utama
+double cariKecepatan(double, double);
+
+// Fungsi konversi
+// Konversi waktu
+double konversiSekonKeJam(double);
+double konversiSekonKeMenit(double);
+double konversi_jam_menit(double);
+double jam_detik(double);
+double menit_detik(double);
+double konversi_menit_jam(double);
+
+// Konversi jarak
+double konversi_km_m(double);
+double konversi_m_km(double);
+
+// Konversi kecepatan
+double kphToMps(double);
+double konversiMpsKeKmph(double);
--- src/kecepatan/pustaka.cpp
+++ src/kecepatan/pustaka.cpp
@@ -1,21 +1,83 @@
 #include "header.h"
 
-float konversiSekonKeJam (float x)
+// Fungsi utama
+double cariKecepatan(double jarak, double waktu)
 {
-    float hasil = x / 3600;
+    double hasil = jarak / waktu;
     return hasil;
 }
 
-float konversiSekonKeMenit (float x)
+double carijarak(double kecepatan, double waktu)
 {
-    float hasil = x / 60;
+    double hasil = kecepatan * waktu;
     return hasil;
 }
 
-float cariKecepatan(float jarak, float waktu)
+double hitung_waktu(double km, double jam)
 {
-    float hasil;
+    double waktu = km / jam;
+    return waktu;
+}
 
-    hasil = jarak / waktu;
+// Fungsi konversi
+// Konversi waktu
+double konversiSekonKeJam (double x)
+{
+    double hasil = x / 3600;
     return hasil;
+}
+
+double konversiSekonKeMenit (double x)
+{
+    double hasil = x / 60;
+    return hasil;
+}
+
+double konversi_jam_menit(double jam)
+{
+    double menit = jam * 60;
+    return menit;
+}
+
+double jam_detik(double jam)
+{
+    jam = jam * 3600;
+    return jam;   
+}
+
+double menit_detik(double menit)
+{
+    menit =menit * 60;
+    return menit;   
+}
+
+double konversi_menit_jam(double menit)
+{
+    double jam = menit / 60;
+    return jam;
+}
+
+// Konversi jarak
+double konversi_km_m(double x)
+{
+    double hasil = x * 1000;
+    return hasil;
+}
+
+double konversi_m_km(double x)
+{
+    double hasil = x / 1000;
+    return hasil;
+}
+
+// Konversi kecepatan
+double kphToMps(double kph)
+{
+    return kph * 1000 / 3600;
+}
+
+double konversiMpsKeKmph(double mps)
+{
+    double kmph = mps * 3.6;
+    return kmph;
 }
